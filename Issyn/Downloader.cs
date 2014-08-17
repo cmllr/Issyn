940e1134470f15054b49d016b7e714f8663a4703
squarerootfury <fury224@googlemail.com>
2014-08-17 16:48:56 +0200

    Fix when the download status code can not be determined

--- Issyn/Downloader.cs
+++ Issyn/Downloader.cs
@@ -23,19 +23,27 @@ namespace Issyn2
 				RunParameters.RequestCount++;	
 				int delay = new Random(DateTime.Now.Millisecond).Next(Properties.CrawlDelay,Properties.MaxCrawlDelay);
 				Thread.Sleep (delay*1000);
-				Output.Print(string.Format("[I]: Waiting {0} seconds as sheduled",delay),false);
+				Output.Print(string.Format("[I]: Download of {0} finished. Waiting {1} seconds as sheduled. Used {2} request until now.",url, delay,RunParameters.RequestCount),false);
 				return content;
 			}
 			catch(WebException wx){
-				HttpStatusCode result =  ((HttpWebResponse)wx.Response).StatusCode;
-				if (new Politeness (RunParameters.AcceptedErrorStatus).IsOK (result)) {
-					Output.Print (string.Format ("[E]: Request failed with {0} on {1}, but status code can be handled.", result.ToString(), url.ToString ()), true);		
-				} else {
-					Output.Print (string.Format ("[E]: request failed with {0} on {1}. Waiting 10 seconds", result.ToString(), url.ToString ()), true);		
+				try{
+					HttpStatusCode result =  ((HttpWebResponse)wx.Response).StatusCode;
+					if (new Politeness (RunParameters.AcceptedErrorStatus).IsOK (result)) {
+						Output.Print (string.Format ("[E]: Request failed with {0} on {1}, but status code can be handled.", result.ToString(), url.ToString ()), true);		
+					} else {
+						Output.Print (string.Format ("[E]: request failed with {0} on {1}. Waiting 10 seconds", result.ToString(), url.ToString ()), true);		
+						RunParameters.CurrentFails++;
+						Thread.Sleep (10000);
+					}
+					return string.Empty;
+				}
+				catch(Exception inner){
+					Output.Print (string.Format ("[E]: request failed with {0} on {1}. Waiting 10 seconds", inner.Message, url.ToString ()), true);		
 					RunParameters.CurrentFails++;
 					Thread.Sleep (10000);
+					return string.Empty;
 				}
-				return string.Empty;
 			}
 			catch (Exception ex){
 				Output.Print (string.Format("[E]: {0} on {1}. Waiting 10 seconds",ex.Message,url.ToString()),true );	
